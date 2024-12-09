5adc05284c99f654b1582cdacbebdfc922ba9b38
Nathan McCormack <G00414780@atu.ie>
2024-12-09 17:00:40 +0000

    Added more  Data to the Transfers Class and Created GET/POST in Service Class

--- src/main/java/ie/atu/TransferService.java
+++ src/main/java/ie/atu/TransferService.java
@@ -1,7 +1,25 @@
 package ie.atu;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TransferService {
+
+    List<Transfers> transfersList = new ArrayList<>();
+
+    @GetMapping("/getTransfers")
+    public List<Transfers> getAllTransfers(){
+        return transfersList;
+    }
+
+    @PostMapping("/addTransfer")
+    public String addTransfer(@RequestBody @Valid Transfers transferRequest){
+        transfersList.add(transferRequest);
+        return "Transfer Details Added";
+    }
 }
