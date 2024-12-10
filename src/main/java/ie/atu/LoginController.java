aed4df1929709bbcb3d1fed6e15a9cbf4132e48d
G00418325@atu.ie <G00418325@atu.ie>
2024-12-10 11:38:31 +0000

    microservice and talend api working

--- src/main/java/ie/atu/LoginController.java
+++ src/main/java/ie/atu/LoginController.java
@@ -5,23 +5,22 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.beans.factory.annotation.Autowired;
 @RequestMapping("/Login")
 @RestController
 public class LoginController {
-    private final LoginService loginService;
+    @Autowired
+    private LoginService loginService;
 
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
     }
 
     @GetMapping("/{username}")
-    public ResponseEntity<?> getUser(@PathVariable String username) {
-        if (username.isBlank()) {
-            return ResponseEntity.badRequest().body("Username cannot be blank");
-        }
+    public ResponseEntity<?> getAllLogins(@PathVariable String loginid) {
 
-        Login login = loginService.getLoginByUsername(username);
+
+        Login login = loginService.getLoginByid(loginid);
 
         if (login == null) {
             return ResponseEntity.notFound().build();
@@ -30,11 +29,13 @@ public class LoginController {
         return ResponseEntity.ok(login);
     }
 
+
     @PostMapping("/createLogin")
     public ResponseEntity<String> createLogin(@Valid @RequestBody Login login) {
         loginService.saveLogin(login);
         return new ResponseEntity<>("User created successfully", HttpStatus.OK);
     }
+
 
 
 }
--- src/main/java/ie/atu/LoginRepository.java
+++ src/main/java/ie/atu/LoginRepository.java
@@ -3,4 +3,6 @@ package ie.atu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginRepository extends JpaRepository<Login,Long> {
+
+    Login findByLoginid(String loginid);
 }
